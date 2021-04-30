ec1ce768c2840fe3baefc0c81c9b84389fd4e101
Ziad Elabd <ziad.elabd91@gmail.com>
2021-04-30 21:55:34 +0200

    adding test cases

--- src/Unit_Test/Test_Essential.java
+++ src/Unit_Test/Test_Essential.java
@@ -16,25 +16,76 @@ import java.io.StringReader;
  * @author Ziad Elabd
  * @author Ziad Albana
  */
+
 public class Test_Essential {	
     
     @Test
-    public void test1() throws ParseException  {
-    	String test="if tt then a:==5 else skip";
+    public void test1() {   // valid case
+    	String test="a := 5";
     	BufferedReader reader = new BufferedReader(new StringReader(test));
         assertTrue(Grammer.check(reader));
     }
     
-   /* 
-    @Test (expected=java.lang.NullPointerException.class)
-    public void testDequeue() {
-    	
+    @Test
+    public void test2() {  // valid case
+    	String test="tt ^ ff";
+    	BufferedReader reader = new BufferedReader(new StringReader(test));
+        assertTrue(Grammer.check(reader));
     }
     
-    @Test (expected=java.lang.NullPointerException.class)
-    public void testFront() {
-          
-    }*/
-
+    @Test
+    public void test3() {   // valid case
+    	String test="(5 + 5)";
+    	BufferedReader reader = new BufferedReader(new StringReader(test));
+        assertTrue(Grammer.check(reader));
+    }
     
+    @Test
+    public void test4() {   // valid case
+    	String test="(5 - 5)";
+    	BufferedReader reader = new BufferedReader(new StringReader(test));
+        assertTrue(Grammer.check(reader));
+    }
+    
+    @Test
+    public void test5() {   // valid case
+    	String test="if tt then a:=5 else skip";
+    	BufferedReader reader = new BufferedReader(new StringReader(test));
+        assertTrue(Grammer.check(reader));
+    }
+    
+    @Test
+    public void test6() {   // valid case
+    	String test="if ff then a:=5 else a:=7";
+    	BufferedReader reader = new BufferedReader(new StringReader(test));
+        assertTrue(Grammer.check(reader));
+    }
+    
+    @Test
+    public void test7() {   // invalid case
+    	String test="if true then a:=5 else skip";
+    	BufferedReader reader = new BufferedReader(new StringReader(test));
+        assertFalse(Grammer.check(reader));
+    }
+    
+    @Test
+    public void test8() {   // invalid case
+    	String test="if false then a:=5 else skip";
+    	BufferedReader reader = new BufferedReader(new StringReader(test));
+    	assertFalse(Grammer.check(reader));
+    }
+    
+    @Test
+    public void test9() {   // invalid case
+    	String test="if tt then a:==5 else skip";
+    	BufferedReader reader = new BufferedReader(new StringReader(test));
+    	assertFalse(Grammer.check(reader));
+    }
+    
+    @Test
+    public void test10() {   // invalid case
+    	String test="if tt then a:=5 else skip;";
+    	BufferedReader reader = new BufferedReader(new StringReader(test));
+    	assertFalse(Grammer.check(reader));
+    }
 }
