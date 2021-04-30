1b0ec108797443c705963e6a8008ef72c714df54
Ziad Elabd <ziad.elabd91@gmail.com>
2021-04-30 14:37:07 +0200

    add test package

---
+++ src/Unit_Test/Test_Essential.java
@@ -0,0 +1,30 @@
+package Unit_Test;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ziad Elabd
+ * @author Ziad Albana
+ */
+public class Test_Essential {	
+    @Test
+    public void testEnqueue() {
+        
+    }
+    
+    
+    @Test (expected=java.lang.NullPointerException.class)
+    public void testDequeue() {
+    	
+    }
+    
+    @Test (expected=java.lang.NullPointerException.class)
+    public void testFront() {
+          
+    }
+
+    
+}
